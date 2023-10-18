8b0184b2547f37a284abed33687c1255918c80d7
dreaMykj <gabrielgreghi2018@gmail.com>
2023-10-18 11:02:45 -0300

    Concluido

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿int n1, n2, soma;
+Console.Write("Digite o primeiro número: ");
+n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o segundo número: ");
+n2 = Convert.ToInt32(Console.ReadLine());
+
+soma = n1 + n2;
+Console.WriteLine($"Soma: {soma} ");
